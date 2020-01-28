6a9ebe0c4f8f5417c846bc08fd09524ddb345e63
Terry Roy <terryr518@gmail.com>
2020-01-28 14:33:15 -0500

    update object.h to match standard repo from piazza
    link for repo: https://github.com/chasebish/cwc_object_string/blob/master/object.h

--- object.h
+++ object.h
@@ -1,16 +1,27 @@
+//lang::CwC
 #pragma once
 
+#include <cstdlib>
+
+/**
+ * A class that represents the top of the object hierarchy.
+ * author: chasebish */
 class Object {
-    public:
-        Object() {}
+public:
+  /** CONSTRUCTORS & DESTRUCTORS **/
 
-        // copy constructor
-        Object(Object* e) {}
+  /* Default Object constructor */
+  Object();
 
-        ~Object() {}
+  /* Default Object destructor, to be overriden by subclasses */
+  virtual ~Object();
 
-        // compares hashes to check for equality
-        bool equals(Object* e) {}
 
-        size_t hash() {}
+  /** VIRTUAL METHODS **/
+
+  /* Returns whether two objects are equal, to be overriden by subclasses */
+  virtual bool equals(Object* const obj);
+
+  /* Returns an object's hash value. Identical objects should have identical hashes */
+  virtual size_t hash();
 };
