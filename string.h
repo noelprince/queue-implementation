6205c3973bee18904dcd6d722771fe26815e88a3
Terry Roy <terryr518@gmail.com>
2020-01-28 14:35:11 -0500

    update string.h to match standard from piazza
    repo link: https://github.com/chasebish/cwc_object_string/blob/master/string.h

--- string.h
+++ string.h
@@ -1,22 +1,49 @@
+//lang::CwC
 #pragma once
 
 #include "object.h"
+#include <cstdlib>
+#include <cstring>
+#include <cstdio> 
 
+/**
+ * An immutable String class representing a char*
+ * author: chasebish */
 class String : public Object {
-    public:
-        String(const char* s) {}
+public:
+  /** CONSTRUCTORS & DESTRUCTORS **/
 
-        String(String* e) {}
+  /* Creates a String copying s */
+  String(const char* s);
 
-        ~String() {}
+  /* Copies a String copying the value from s */
+  String(String* const s);
 
-        // returns the length of the string (in characters)
-        size_t length() {}
+  /* Clears String from memory */
+  ~String();
 
-        // compares strings (in the same manner as strcmp)
-        int compare(String* e) {}
 
-        bool equals(Object* e) {}
+  /** INHERITED METHODS **/
 
-        size_t hash() {}
-}
+  /* Inherited from Object, generates a hash for a String */
+  size_t hash();
+
+  /* Inherited from Object, checks equality between an String and an Object */
+  bool equals(Object* const obj);
+
+
+  /** STRING METHODS **/
+  
+  /** Compares strings based on alphabetical order
+   * < 0 -> this String is less than String s
+   * = 0 -> this String is equal to String s
+   * > 0 -> this String is greater than String s
+   */
+  int cmp(String* const s);
+
+  /* Creates a new String by combining two existing Strings */
+  String* concat(String* const s);
+
+  /* Returns the current length of the String */
+  size_t size();
+};
